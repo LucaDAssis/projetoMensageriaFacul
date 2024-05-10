c98d3838f531d80c6d7901d93909454e8667ee2a
Matheus dos Santos <matheusdossantos3157@gmail.com>
2024-05-10 02:57:55 -0300

    trazendo bootstrap para o header

--- templates/header.php
+++ templates/header.php
@@ -1,29 +1,43 @@
 <?php
     include_once("config/urlBase.php");
     include_once("config/processar_formulario.php");
-
 ?>
-
-
-
 
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <<link rel="stylesheet" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!--<<link rel="stylesheet" href="styles.css">-->
     <title>projetofacul</title>
 </head>
 <body>
 <header>
-        <!--<h1>Javeiros</h1>-->
-        <nav>
-            <ul>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="sobre.php">Sobre</a></li>
-                <li><a href="servicos.php">Serviços</a></li>
-                <li><a href="contato.php">Contato</a></li>
-            </ul>
-        </nav>
+  <nav class="navbar navbar-expand-lg navbar-dark" style="background-image: linear-gradient(to top, #240321, #480541);"> 
+    <div class="container-fluid">
+      <a class="navbar-brand me-4" href="#" style="font-size: 30px;">Javeiros</a> <!-- Adicionando margem à direita -->
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item me-sm-4">
+            <a class="nav-link active" aria-current="page" href="index.php" style="font-size: 20px;">Home</a>
+          </li>
+          <li class="nav-item me-sm-4">
+            <a class="nav-link active" aria-current="page" href="sobre.php" style="font-size: 20px;">Sobre</a>
+          </li>
+          <li class="nav-item me-sm-4">
+            <a class="nav-link active" aria-current="page" href="servicos.php" style="font-size: 20px;">Serviços</a>
+          </li>
+          <li class="nav-item me-sm-4">
+            <a class="nav-link active" aria-current="page" href="contato.php" style="font-size: 20px;">Contato</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </header>
+</body>
+</html>
