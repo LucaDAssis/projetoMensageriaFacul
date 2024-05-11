4c20cc32140d705f8681bb591730669e3d676db2
Matheus dos Santos <matheusdossantos3157@gmail.com>
2024-05-10 22:01:28 -0300

    feat: serviços com front end

--- servicos.php
+++ servicos.php
@@ -4,15 +4,16 @@
 
 ?>
 
-<h1 id="servico">
-    <p>Serviços</p>
-</h1>
-
 <div id="paragrafo">
+    <h1>Serviços</h1>
     <p>Desenvolvimento de API's em JAVA? CONTA COM A GENTE</p>
+    <img src="./images/feliz.jpg" alt="garoto feliz" width="50%">
     <p>Precisa de pessoas com conhecimento em AWS? CONTA COM A GENTE</p>
+    <img src="./images/feliz.jpg" alt="garoto feliz" width="50%">
     <p>Precisa de pessoas com experiência em microserviços? CONTA COM A GENTE</p>
-    <p>Precisa criar um designer de um site? não conte com a gente...</p>
+    <img src="./images/feliz.jpg" alt="garoto feliz" width="50%">
+    <p>Precisa criar um designe de um site? não conte com a gente...</p>
+    <img src="./images/triste.png" alt="garoto triste" width="30%" style="margin-left: 100px;">
 </div>
 
 <?php
