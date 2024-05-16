f4e57937c56a348f6f0b0b16950e6e01f0a53019
Matheus dos Santos <matheusdossantos3157@gmail.com>
2024-05-15 21:38:52 -0300

    fix: ajustando  front end

--- servicos.php
+++ servicos.php
@@ -5,16 +5,16 @@
 ?>
 <body class="pagina-servicos">
     <div id="servicos">
-        <h1>Serviços</h1>
-        <p>Desenvolvimento de API's em JAVA? CONTA COM A GENTE</p>
+        <h1 style = "text-align: center;">Serviços</h1>
+        <p style = "text-align: center;">Desenvolvimento de API's em JAVA? CONTA COM A GENTE</p>
         <img src="./images/feliz.png" alt="garoto feliz" width="50%">
-        <p>Precisa de pessoas com conhecimento em AWS? CONTA COM A GENTE</p>
+        <p style = "text-align: center;">Precisa de pessoas com conhecimento em AWS? CONTA COM A GENTE</p>
         <img src="./images/feliz.png" alt="garoto feliz" width="50%">
-        <p>Precisa de pessoas com experiência em microserviços? CONTA COM A GENTE</p>
+        <p style = "text-align: center;">Precisa de pessoas com experiência em microserviços? CONTA COM A GENTE</p>
         <img src="./images/feliz.png" alt="garoto feliz" width="50%">
-        <p>Precisa criar um design de um site? não conte com a gente...</p>
+        <p style = "text-align: center;">Precisa criar um design de um site? não conte com a gente...</p>
         <img src="./images/triste.png" alt="garoto triste" width="37%" style="margin-left: 70px;">
-        <p>E se  você insistir no design...</p>
+        <p style = "text-align: center;">E se  você insistir no design...</p>
         <img src="./images/puto.png" alt="garoto puto" width="55%" style="margin-left: -20px;">
     </div>
 </body>
