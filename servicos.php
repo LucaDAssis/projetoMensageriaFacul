038cad5590b735dc5dd44a9f8660ede77873743d
Matheus dos Santos <matheusdossantos3157@gmail.com>
2024-05-21 16:59:59 -0300

    fix: centralizando as imagens do serviços

--- servicos.php
+++ servicos.php
@@ -1,21 +1,19 @@
 <?php
-
     include_once("templates/header.php");
-
 ?>
 <body class="pagina-servicos">
     <div id="servicos">
-        <h1 style = "text-align: center;">Serviços</h1>
-        <p style = "text-align: center;">Desenvolvimento de API's em JAVA? CONTA COM A GENTE</p>
-        <img src="./images/feliz.png" alt="garoto feliz" width="50%">
-        <p style = "text-align: center;">Precisa de pessoas com conhecimento em AWS? CONTA COM A GENTE</p>
-        <img src="./images/feliz.png" alt="garoto feliz" width="50%">
-        <p style = "text-align: center;">Precisa de pessoas com experiência em microserviços? CONTA COM A GENTE</p>
-        <img src="./images/feliz.png" alt="garoto feliz" width="50%">
-        <p style = "text-align: center;">Precisa criar um design de um site? não conte com a gente...</p>
-        <img src="./images/triste.png" alt="garoto triste" width="37%" style="margin-left: 70px;">
-        <p style = "text-align: center;">E se  você insistir no design...</p>
-        <img src="./images/puto.png" alt="garoto puto" width="55%" style="margin-left: -20px;">
+        <h1 style="text-align: center;">Serviços</h1>
+        <p style="text-align: center;">Desenvolvimento de API's em JAVA? CONTA COM A GENTE</p>
+        <img src="./images/feliz.png" alt="garoto feliz" width="50%" class="center-img">
+        <p style="text-align: center;">Precisa de pessoas com conhecimento em AWS? CONTA COM A GENTE</p>
+        <img src="./images/feliz.png" alt="garoto feliz" width="50%" class="center-img">
+        <p style="text-align: center;">Precisa de pessoas com experiência em microserviços? CONTA COM A GENTE</p>
+        <img src="./images/feliz.png" alt="garoto feliz" width="50%" class="center-img">
+        <p style="text-align: center;">Precisa criar um design de um site? não conte com a gente...</p>
+        <img src="./images/triste.png" alt="garoto triste" width="37%" class="center-img">
+        <p style="text-align: center;">E se  você insistir no design...</p>
+        <img src="./images/puto.png" alt="garoto puto" width="55%" class="center-img">
     </div>
 </body>
 <?php
