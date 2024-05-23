dd170d605b66dc616ef71e4d855864d4bacd20c5
Matheus dos Santos <matheusdossantos3157@gmail.com>
2024-05-23 04:57:11 -0300

    feat: DesignTeste

--- config/thanks.php
+++
@@ -1,6 +0,0 @@
-<h1>
-    <P>Muito obrigada por nós enviar uma mensagem!</P>
-</h1>
-<h2><p>Responderemos você o mais rápido possível, pelo seu email!
-    Fique ligado, ein. =)</p>
-</h2>
--- config/urlBase.php
+++
@@ -1,5 +0,0 @@
-<?php
-$protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https://" : "http://";
-$host = $_SERVER['HTTP_HOST'];
-$base_url = $protocol . $host . 'http://localhost:63342/projetoMensageriaFacul/sobre.php';
-?>
